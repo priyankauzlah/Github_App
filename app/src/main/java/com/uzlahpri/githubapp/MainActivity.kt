33d14725f40bbaf293adbcb377e7cc2a844859a6
priyanka uzlah <priyankauzlah@gmail.com>
2021-08-27 15:14:48 +0700

    Search Bar

--- app/src/main/java/com/uzlahpri/githubapp/MainActivity.kt
+++ app/src/main/java/com/uzlahpri/githubapp/MainActivity.kt
@@ -1,34 +1,113 @@
 package com.uzlahpri.githubapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
-import com.uzlahpri.githubapp.databinding.ActivitySplashBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.uzlahpri.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainBinding: ActivitySplashBinding
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var listUser: ArrayList<Users>
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        setUserAdapter()
+        showProgressBar(true)
+        setViewModel()
+    }
+
+    private fun setViewModel() {
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+            MainViewModel::class.java
+        )
+        if (mainViewModel.getListUser().value == null) {
+            mainViewModel.setListUser(this, "")
+        }
+        mainViewModel.getListUser().observe(this, { users ->
+            if (users != null) {
+                listUser = users
+                userAdapter.setData(listUser)
+                showProgressBar(false)
+            }
+        })
+    }
+
+    private fun showProgressBar(b: Boolean) {
+        if (b) {
+            mainBinding.pbMain.visibility = View.VISIBLE
+        } else {
+            mainBinding.pbMain.visibility = View.GONE
+        }
+    }
+
+    private fun setUserAdapter() {
+        userAdapter = UserAdapter()
+        userAdapter.notifyDataSetChanged()
+
+        mainBinding.rvMain.apply {
+            setHasFixedSize(true)
+            adapter = userAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
 
         val search = menu?.findItem(R.id.ic_search)
-        search?.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+        search?.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
             override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
                 return true
             }
 
             override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
-                TODO("Not yet implemented")
+                showProgressBar(true)
+                mainViewModel.setListUser(this@MainActivity, "")
+                return true
             }
 
         })
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.ic_search)?.actionView as androidx.appcompat.widget.SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = "Search Username"
+        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                showProgressBar(true)
+                mainViewModel.setListUser(this@MainActivity, p0)
+                closeKeyboard()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun closeKeyboard() {
+        val view: View? = this.currentFocus
+
+        if (view != null) {
+            val iMM: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            iMM.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
--- app/src/main/java/com/uzlahpri/githubapp/MainViewModel.kt
+++ app/src/main/java/com/uzlahpri/githubapp/MainViewModel.kt
@@ -1,14 +1,20 @@
 package com.uzlahpri.githubapp
 
 import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+import org.json.JSONObject
 
-class MainViewModel : ViewModel(){
+class MainViewModel : ViewModel() {
     val listUser = MutableLiveData<ArrayList<Users>>()
 
-    fun setListUser(context: Context, query: String? = null){
+    fun setListUser(context: Context, query: String? = null) {
         val users = ArrayList<Users>()
 
         //request ke server
@@ -16,9 +22,58 @@ class MainViewModel : ViewModel(){
         client.addHeader("Authorization", "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")
         client.addHeader("User-Agent", "request")
 
-        val url = when(query){
+        val url = when (query) {
             "" -> "https://api.github.com/users"
             else -> "https://api.github.com/search/users?q=$query"
         }
+
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray
+            ) {
+                try {
+                    val result = String(responseBody)
+                    val jsonArray = when (query) {
+                        "" -> JSONArray(result)
+                        else -> JSONObject(result).getJSONArray("items")
+                    }
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+
+                        val user = Users()
+                        user.name = jsonObject.getString("login")
+                        user.username = jsonObject.getString("login")
+                        user.avatar = jsonObject.getString("avatar_url")
+
+                        users.add(user)
+                    }
+                    listUser.postValue(users)
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable
+            ) {
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+    fun getListUser(): LiveData<ArrayList<Users>>{
+        return listUser
     }
 }
